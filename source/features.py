e41ac44f1ecb9443f9e211991077d0185ca8ccc3
BruhBoba <palananya801@gmail.com>
2026-07-26 19:15:20 -0500

    cleaned values for feature selection

--- source/features.py
+++ source/features.py
@@ -17,55 +17,45 @@ df = df.sort_values(by='timestamp')
 # new col to hold past timestamp's util rate
 df['util_30_mins_ago'] = df['utilization_rate'].shift(1)
 
-# drop first row, cuz util rate is NaN atp
+# remove first row cuz new col is empty atp
 df = df.dropna(subset=['util_30_mins_ago'])
 
+# remove features that cause leakage or r unnecessary
+drop_cols = [
+    "timestamp", "station_id", "station_name", "city", "state",                 
+    "ports_total", "ports_available", "ports_occupied", 
+    "ports_out_of_service", "estimated_wait_time_mins", 
+    "current_price", "pricing_type", "station_status", 
+    "latitude", "longitude"
+]
+
+# make new df
+df_model = df.drop(columns=drop_cols)
+
 # assign x and y vals
-y = df['utilization_rate']
-X = df.drop(columns=['utilization_rate', 'timestamp'])
+y = df_model['utilization_rate']
+x = df_model.drop(columns=['utilization_rate'])
+
 
 # split data into 80/20 train/test
 # REMOVE RANDOM STATE PARAM B4 DEPLOYING
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=117)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=117)
 
-print(f"Training data shape: {X_train.shape}")
-print(f"Testing data shape: {X_test.shape}")
+# properly encodes amenities feature which has various elements in one long string
+amenities_encoded = x_train['amenities'].str.get_dumies(sep=', ')
+x_train  = x_train.drop(columns=['amenities'])
+x_train = pd.concat([x_train, amenities_encoded], axis = 1)
+
+# encodes other non-numeric values
+x_train_encoded = pd.get_dummies(x_train)
 
 
-
-plt.style.use('seaborn-v0_8-whitegrid')
-plt.rcParams['figure.figsize'] = (12, 6)
-plt.rcParams['font.size'] = 11
-
-
-# Remove features that cause leakage or are not useful
-drop_cols = [
-    "timestamp", 
-    "station_id",
-    "station_name",
-    "city",
-    "state",                
-    "ports_total",
-    "ports_available",
-    "ports_occupied",
-    "ports_out_of_service",
-    "estimated_wait_time_mins",
-    "current_price",
-    "pricing_type"
-]
-
-df_model = df.drop(columns=drop_cols)
-print(df_model.columns)
-
-# Select only numeric columns
-numeric_df = df_model.select_dtypes(include=["int64", "float64", "bool"])
-
-# Pearson correlation
+# pearson correlation: checks 4 linear relationships
 pearson = numeric_df.corr(method="pearson")
 # Sort correlations with utilization_rate
 pearson_target = pearson["utilization_rate"].sort_values(ascending=False)
 
-# Spearman correlation
+# spearman correlation: checks for any trends (monotonic)
 spearman = numeric_df.corr(method="spearman")
 spearman_target = spearman["utilization_rate"].sort_values(ascending=False)
 
